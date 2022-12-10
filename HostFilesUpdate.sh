3c129ddfba011319beae5dcd259c56e3a53dcba4
Franck M <18901560+airphel@users.noreply.github.com>
2022-12-10 10:25:46 +0100

    Add files via upload

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -6,7 +6,7 @@
 #      Written for Pi-Star (http://www.pistar.uk/)      #
 #               By Andy Taylor (MW0MWZ)                 #
 #                  Enhanced by W0CHP                    #
-#                    Version 2.10.4                     #
+#                    Version 2.10.5                     #
 #                                                       #
 #   Based on the update script by Tony Corbett G0WFV    #
 #                                                       #
@@ -23,7 +23,7 @@ dashVer=$( git --work-tree=/var/www/dashboard --git-dir=/var/www/dashboard/.git 
 psVer=$( grep Version /etc/pistar-release | awk '{print $3}' )
 # hostFileURL=https://hostfiles.w0chp.net
 hostFileURL=https://raw.githubusercontent.com/airphel/WPSD-HostFiles/main
-uuidStr=$(grep UUID /etc/pistar-release | awk {'print $3'})
+uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | awk {'print $3'} | tac | xargs| sed 's/ /_/g')
 modelName=$(grep -m 1 'model name' /proc/cpuinfo | sed 's/.*: //')
 hardwareField=$(grep 'Model' /proc/cpuinfo | sed 's/.*: //')
 hwDeetz="${hardwareField} - ${modelName}"
